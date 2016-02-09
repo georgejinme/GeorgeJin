3d56f10a1adc736f9efbb9c07907849df8729722
钩钩么么哒 <a516550257@qq.com>
2016-02-09 15:11:23 +0800

    pjax

--- js/gjin.js
+++ js/gjin.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
+    NProgress.start()
+    initProgress()
 	initPath()
     initTitle()
-    initNavTrigger()
     initNav()
-    initFrameListener()
+    initNavItems()
 })
+
+window.onload = function(){ 
+    NProgress.done()
+}
+
+var initProgress = function() {
+    $(document).on('pjax:start', function() { NProgress.start(); });
+    $(document).on('pjax:end',   function() { NProgress.done();  });
+}
 
 
 
@@ -131,6 +141,7 @@ var initPath = function(){
 }
 
 var initTitle = function() {
+    $(".myProjectBtn").hide()
     $(".fadeInLeft").textillate({
         initialDelay: 500,
         in: {
@@ -142,7 +153,9 @@ var initTitle = function() {
     })
 }
 
-var initNavTrigger = function() {
+
+var initNav = function() {
+
     var pathA = document.getElementById('pathA'),
     pathC = document.getElementById('pathC'),
     segmentA = new Segment(pathA, 8, 32),
@@ -171,25 +184,6 @@ var initNavTrigger = function() {
 
     function outB(s) { s.draw(8, 32, 0.7, {delay: 0.1, easing: ease.ease('elastic-out', 2, 0.4)}); }
 
-    var trigger = $('#menu-icon-trigger')
-    var toCloseIcon = true;
-
-    trigger.click(function(){
-        if (toCloseIcon) {
-            inAC(segmentA);
-            inB(segmentB);
-            inAC(segmentC);
-        } else {
-            outAC(segmentA);
-            outB(segmentB);
-            outAC(segmentC);
-        }
-        toCloseIcon = !toCloseIcon
-    })
-}
-
-
-var initNav = function() {
     function mobilecheck() {
         var check = false;
         (function(a){if(/(android|ipad|playbook|silk|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
@@ -203,6 +197,9 @@ var initNav = function() {
             // event type (if mobile, use touch events)
             eventtype = mobilecheck() ? 'touchstart' : 'click',
             resetMenu = function() {
+                outAC(segmentA);
+                outB(segmentB);
+                outAC(segmentC);
                 classie.remove( menu, 'bt-menu-open' );
                 classie.add( menu, 'bt-menu-close' );
             },
@@ -223,6 +220,9 @@ var initNav = function() {
                 resetMenu();
             }
             else {
+                inAC(segmentA);
+                inB(segmentB);
+                inAC(segmentC);
                 classie.remove( menu, 'bt-menu-close' );
                 classie.add( menu, 'bt-menu-open' );
                 overlay.addEventListener( eventtype, closeClickFn );
@@ -234,33 +234,12 @@ var initNav = function() {
     init();
 }
 
-var initFrameListener = function(){
-    var stats = new Stats();
-    stats.setMode( 0 ); // 0: fps, 1: ms, 2: mb
-
-    // align top-left
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.left = '30px';
-    stats.domElement.style.top = '30px';
-
-    document.body.appendChild( stats.domElement );
-
-    var update = function () {
-
-        stats.begin();
-
-        // monitored code goes here
-
-        stats.end();
-
-        requestAnimationFrame( update );
-
-    };
-
-    requestAnimationFrame( update );
+var initNavItems = function(){
+    $("#homeNav, #meNav").click(function(){
+        $(document).pjax("a", ".container", {fragment: ".container"})
+        $('#menu-icon-trigger').trigger("click")
+    })
 }
-
-
 
 
 
